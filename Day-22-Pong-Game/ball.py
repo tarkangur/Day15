4a9201f068e8bf294f9aea06e3f1e94057b38367
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-11 23:14:36 +0300

    Update ball.py

--- Day-22-Pong-Game/ball.py
+++ Day-22-Pong-Game/ball.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+import random
+STARTING_HEAD = [45, -45, 135, -135]
 
 
 class Ball(Turtle):
@@ -7,25 +9,59 @@ class Ball(Turtle):
         self.penup()
         self.color("white")
         self.shape("circle")
+        self.setheading(random.choice(STARTING_HEAD))
+        self.move_speed = 0.1
+
+    def paddle_hit(self):
+        x = self.xcor()
+        y = self.ycor()
+        if x + 10 > 330 and self.heading() == 45:
+            x -= 10
+            y += 10
+            self.setheading(135)
+        elif x + 10 > 330 and self.heading() == 315:
+            x -= 10
+            y -= 10
+            self.setheading(225)
+        elif x - 10 < -330 and self.heading() == 225:
+            x += 10
+            y -= 10
+            self.setheading(315)
+        elif x - 10 < -330 and self.heading() == 135:
+            x += 10
+            y += 10
+            self.setheading(45)
+        self.move_speed *= 0.9
 
     def move(self):
         x = self.xcor()
         y = self.ycor()
-        if y + 10 > 290:
-            new_x = x + 10
-            new_y = y - 10
-            self.setheading(-45)
-            self.forward(10)
-        elif y - 10 < - 290:
-            new_x = x + 10
-            new_y = y + 10
+        if y + 10 > 290 and self.heading() == 45:
+            x += 10
+            y -= 10
+            self.setheading(315)
+        elif y - 10 < - 290 and self.heading() == 315:
+            x += 10
+            y += 10
             self.setheading(45)
-            self.forward(10)
+        elif y + 10 > 290 and self.heading() == 135:
+            x += 10
+            y -= 10
+            self.setheading(225)
+        elif y - 10 < - 290 and self.heading() == 225:
+            x += 10
+            y += 10
+            self.setheading(135)
         else:
-            new_x = x + 10
-            if self.heading() == 45:
-                new_y = y + 10
-            else:
-                new_y = y - 10
             self.forward(10)
+
+    def restart_l(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.setheading(random.choice(STARTING_HEAD[2:4]))
+
+    def restart_r(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.setheading(random.choice(STARTING_HEAD[0:2]))
 
