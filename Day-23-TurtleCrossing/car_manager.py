e4572e7c1cc924d8f1846fd103fe79c1bfdbe407
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-12 21:24:15 +0300

    Add files via upload

---
+++ Day-23-TurtleCrossing/car_manager.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.setheading(180)
+        self.penup()
+
+    def car_move(self):
+        self.forward(MOVE_INCREMENT)
+
