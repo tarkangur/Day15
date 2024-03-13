9ecff0f38594e2a1f221419cc652a1baab5011f0
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-14 01:13:57 +0300

    Update main.py

--- Day-20-21-SnakeGame/main.py
+++ Day-20-21-SnakeGame/main.py
@@ -28,19 +28,18 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
-    kontrol = 0
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         score.score_add()
         score.score_write()
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
+
     for i in snake.segments[1:]:
         if snake.head.distance(i) < 10:
-            game_is_on = False
-            score.game_over()
-
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
