75b0440cef3200e91d7411353e4ad2d644bf4c8a
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-13 07:17:58 +0300

    Update main.py

--- Day-23-TurtleCrossing/main.py
+++ Day-23-TurtleCrossing/main.py
@@ -6,16 +6,30 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
 screen.tracer(0)
-user = Player()
-car = CarManager()
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 screen.listen()
-screen.onkeypress(user.move, "Up")
+screen.onkeypress(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    car.car_move()
 
+    car_manager.create_car()
+    car_manager.car_move()
+    if player.ycor() > 290:
+        player.level_up()
+        car_manager.car_level_up()
+        scoreboard.level += 1
+        scoreboard.update_level()
+    for car in car_manager.cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
