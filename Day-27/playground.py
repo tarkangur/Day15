3595bef30d3d1607ef278951fe3d7007d2254855
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-18 23:05:06 +0300

    Add files via upload

---
+++ Day-27/playground.py
@@ -0,0 +1,33 @@
+def add(*args):
+    print(args[0])
+
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+
+# print(add(3, 5, 8))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    # n += kwargs["add"]
+    # n *= kwargs["multiply"]
+    # print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(make="Nissan")
+print(my_car.model)
