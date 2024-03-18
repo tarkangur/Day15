f4ffcd0fdbe340996af490719557ee42470ff517
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-18 23:04:54 +0300

    Create main.py

---
+++ Day-27/main.py
@@ -0,0 +1,27 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "italic"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
