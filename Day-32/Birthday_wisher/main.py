b6b4d49538a16206d6752df27d3df30cef28fb8a
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-03-27 09:48:18 +0300

    Update main.py

--- Day-32/Birthday_wisher/main.py
+++ Day-32/Birthday_wisher/main.py
@@ -3,6 +3,8 @@ import pandas
 import random
 import smtplib
 
+EMAIL = "d41913970@gmail.com"
+PASSWORD = "phbasfovtikzwyds"
 
 now = dt.datetime.now()
 now_month = now.month
@@ -10,44 +12,20 @@ now_day = now.day
 
 data_file = pandas.read_csv("birthdays.csv")
 today_data = data_file.loc[(data_file['day'] == now_day) & (data_file['month'] == now_month)]
+emails = [mail for mail in today_data["email"]]
+names = [name for name in today_data["name"]]
+for person in names:
+    index = names.index(person)
+    email = emails[index]
+    number = random.randint(1, 3)
+    with open(f"letter_templates/letter_{number}.txt", "r") as start_letter:
+        letter = start_letter.read()
+        final_letter = letter.replace("[NAME]", f"{person}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs={email},
+                            msg=f"Subject:Happy Birthday\n\n{final_letter}"
+                            )
