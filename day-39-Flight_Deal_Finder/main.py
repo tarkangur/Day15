ec8b5f73cfe5a0f13d293ad1345b38c569dc401f
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-13 02:09:34 +0300

    Update main.py

--- day-39-Flight_Deal_Finder/main.py
+++ day-39-Flight_Deal_Finder/main.py
@@ -1,19 +1,27 @@
 from data_manager import DataManager
-from flight_data import FlightData
 from flight_search import FlightSearch
-from notification_manager import NotificationManager
+from datetime import datetime, timedelta
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-print(sheet_data)
+flight_search = FlightSearch()
+
+ORIGIN_CITY_IATA = "LON"
 
 if sheet_data[0]["iataCode"] == "":
-    flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_code()
 
-# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=180)
+
+for destination in sheet_data:
+    flight_search.search_flight(
+        origin_city_code=ORIGIN_CITY_IATA,
+        destination_city_code=destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    
