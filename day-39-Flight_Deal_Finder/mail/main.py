15d56003d66f2b8cee3c647b7aea0b262240f0b1
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-15 00:27:26 +0300

    Update main.py

--- day-39-Flight_Deal_Finder/mail/main.py
+++ day-39-Flight_Deal_Finder/mail/main.py
@@ -3,12 +3,17 @@ import sheety
 print("Welcome to Flight Club.\nWe find the best flight deals and email you.")
 name = input("What is your first name?\n").title()
 lastname = input("What is your last name?\n").title()
-email = input("What is your email?\n")
-check_email = input("Type your email again.\n")
-if email == check_email:
-  print("You're in the club!")
-  sheety.post_new_row(name, lastname, email)
-  
-else:
-  print("Wrong email")
-  exit()
+email = "a"
+check_email = "b"
+
+while email != check_email:
+  email = input("What is your email?\n")
+  if email.lower() == "quit" or email.lower() == "exit":
+    exit()
+  check_email = input("Type your email again.\n")
+  if check_email.lower() == "quit" or check_email.lower() == "exit":
+    exit()
+
+print("You're in the club!")
+
+sheety.post_new_row(name, lastname, email)
