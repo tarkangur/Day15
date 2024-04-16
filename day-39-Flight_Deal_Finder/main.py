ce47bca10bcb06bee714924af455c24f67ad54ff
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-16 16:34:04 +0300

    Update main.py

--- day-39-Flight_Deal_Finder/main.py
+++ day-39-Flight_Deal_Finder/main.py
@@ -11,23 +11,38 @@ notification_manager = NotificationManager()
 ORIGIN_CITY_IATA = "LON"
 
 if sheet_data[0]["iataCode"] == "":
-    for row in sheet_data:
-        row["iataCode"] = flight_search.get_destination_code(row["city"])
-    data_manager.destination_data = sheet_data
+    city_names = [row["city"] for row in sheet_data]
+    data_manager.city_codes = flight_search.get_destination_code(city_names)
     data_manager.update_destination_code()
+    sheet_data = data_manager.get_destination_data()
+
+destination = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=180)
 
-for destination in sheet_data:
+for destination_code in destination:
     flight = flight_search.search_flight(
         origin_city_code=ORIGIN_CITY_IATA,
-        destination_city_code=destination["iataCode"],
+        destination_city_code=destination_code,
         from_time=tomorrow,
         to_time=six_month_from_today
     )
 
-    if flight.price < destination["lowestPrice"]:
-        notification_manager.write_message(
-            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}"
-        )
+    if flight is None:
+        continue
+
+    if flight.price < destination[destination_code]["price"]:
+            message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}"
+
+            if flight.stop_overs > 0:
+                message += f"\n Flight has {flight.stop_overs} stop over, via {flight.via_city}"
+                print(message)
+
+            notification_manager.write_message(message)
+
