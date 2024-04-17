c56c21009d2be4b1d710c7e08a1a93c290599fd6
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-17 23:31:50 +0300

    Update notification_manager.py

--- day-39-Flight_Deal_Finder/notification_manager.py
+++ day-39-Flight_Deal_Finder/notification_manager.py
@@ -1,7 +1,10 @@
 from twilio.rest import Client
+import smtplib
 
 account_sid = ""
 auth_token = ""
+my_email = ""
+password = "s"
 
 
 class NotificationManager:
@@ -9,10 +12,22 @@ class NotificationManager:
     def __init__(self):
         self.client = Client(account_sid, auth_token)
 
-    def write_message(self, message):
+    def send_sms(self, message):
         message = self.client.messages \
             .create(
                 body=message,
-                from_='',
-                to=''
+                from_='+12052360362',
+                to='+905054506333'
             )
+        print(message.sid)
+
+    def send_emails(self, message, emails):
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=my_email,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}".encode('utf-8')
+                )
