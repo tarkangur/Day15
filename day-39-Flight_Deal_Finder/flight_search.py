7e377743d4215916ddab83682f0186c64bbd282c
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-18 20:20:51 +0300

    Update flight_search.py

--- day-39-Flight_Deal_Finder/flight_search.py
+++ day-39-Flight_Deal_Finder/flight_search.py
@@ -1,6 +1,5 @@
 import requests
 from flight_data import FlightData
-from pprint import pprint
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 TEQUILA_API_KEY = ""
@@ -8,24 +7,18 @@ TEQUILA_API_KEY = ""
 
 class FlightSearch:
 
-    def __init__(self):
-        self.city_codes = []
-
     def get_destination_code(self, city_name):
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
         headers = {"apikey": TEQUILA_API_KEY}
-        for city in city_name:
-            query = {"term": city, "location_types": "city"}
-            response = requests.get(
-                url=location_endpoint,
-                headers=headers,
-                params=query
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(
+            url=location_endpoint,
+            headers=headers,
+            params=query
             )
-            results = response.json()
-            code = results["locations"][0]["code"]
-            self.city_codes.append(code)
-
-        return self.city_codes
+        results = response.json()
+        code = results["locations"][0]["code"]
+        return code
 
     def search_flight(self, origin_city_code, destination_city_code, from_time, to_time):
         search_endpoint = f"{TEQUILA_ENDPOINT}/v2/search"
@@ -36,9 +29,10 @@ class FlightSearch:
             "date_from": from_time.strftime("%d/%m/%Y"),
             "date_to": to_time.strftime("%d/%m/%Y"),
             "nights_in_dst_from": 7,
-            "nights_in_dst_to": 21,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
             "one_for_city": 1,
-            "max_stopovers": 1,
+            "max_stopovers": 0,
             "curr": "GBP"
         }
 
@@ -58,7 +52,6 @@ class FlightSearch:
                 params=query
             )
             data = response.json()["data"][0]
-            pprint(data)
             flight_data = FlightData(
                 price=data["price"],
                 origin_city=data["route"][0]["cityFrom"],
