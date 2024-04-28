cec11a2a3ef4943648ccb772e007ef5b88e819f0
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-28 21:11:58 +0300

    Create main.py

---
+++ Day-1/band_name_generator/main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Hosgeldiniz")
+#2. Ask the user for the city that they grew up in.
+Sehir = input("Nerede büyüdün?\n")
+#3. Ask the user for the name of a pet.
+hayvan = input("İlk hayvanının adı nedir?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Grup adın: " + Sehir + " " + hayvan)
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
