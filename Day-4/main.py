d83e73751e8df972e37c14b1d3af1d51d6185c36
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-29 14:40:14 +0300

    Create main.py

---
+++ Day-4/main.py
@@ -0,0 +1,37 @@
+# # import random
+
+# # random_integer = random.randint(1,10)
+
+# # print(random_integer)
+
+# # random_float = random.random()
+# # print(random_float)
+
+# # random_float1 = random.uniform(0,5)
+# # print(random_float1)
+
+# states_of_america = [
+#     "Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut",
+#     "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia",
+#     "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky",
+#     "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois",
+#     "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas",
+#     "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas",
+#     "West Virginia", "Nevada", "Nebraska", "Colorad", "North Dakota",
+#     "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah",
+#     "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"
+# ]
+
+# print(states_of_america)
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
