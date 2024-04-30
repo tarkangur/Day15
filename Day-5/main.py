7d5bc7223f703e44b1e8524c79e770a75755a3e0
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-04-30 21:30:48 +0300

    Create main.py

---
+++ Day-5/main.py
@@ -0,0 +1,13 @@
+import random
+# fruits = ["apple", "peach", "pear"]
+
+# for fruit in fruits:
+#   # print(fruit)
+for number in range(0, 10):
+
+ print(number)
+
+total = 0
+for number in range(1, 101):
+ total += number
+print(total)
