37027e299a533d94175099fba1e83a4378ad1f1e
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-05-04 21:33:22 +0300

    Create main.py

---
+++ Day-8/main.py
@@ -0,0 +1,26 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+# def greet():
+#   print("Merhaba")
+#   print("Nasılsın?")
+#   print("Görüşürüz")
+
+# greet()
+
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How are you {name}?")
+
+# greet_with_name("Tarkan")
+
+#Function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+# greet_with("Tarkan", "İstanbul")
+
+greet_with(location="istanbul", name="tarkan")
+
