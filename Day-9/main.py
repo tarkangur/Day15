04ed438d07a1c165e2f257d654e6a6b4b225e809
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-05-06 19:03:16 +0300

    Create main.py

---
+++ Day-9/main.py
@@ -0,0 +1,23 @@
+programming_dictionary = {
+    "Bug":
+    "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Lille", "Dijon"],
+        "total_visit": 12
+    },
+    {
+        "country": "Turkey",
+        "cities_visited": ["İstanbul", "Ankara", "Eskişehir"],
+        "total_visit": 3
+    },
+]
+
+print(programming_dictionary)
+print(travel_log)
