ef866152fdd29deb4225fba9889d830790d24492
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-05-08 16:03:41 +0300

    Create interaction.py

---
+++ Day-48/interaction.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_option)
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+articles = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+# articles.click()
+
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+search = driver.find_element(By.NAME, value="search")
+search.send_keys("Python", Keys.ENTER)
+
