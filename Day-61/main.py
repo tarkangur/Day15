9b9468248dc345c998c23a6b40a4b15606e867bb
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-05-31 04:25:15 +0300

    Update main.py

--- Day-61/main.py
+++ Day-61/main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import PasswordField, EmailField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap5
 import secrets
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email")
-    password = StringField("Password")
+    email = EmailField(label="Email", validators=[DataRequired(), Email(message='Invalid email address')])
+    password = PasswordField(label="Password", validators=[DataRequired(),
+                                                           Length(min=8,
+                                                                  message='Field must be at least 8 characters long.')])
+    submit = SubmitField(label="Log In")
 
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+
+bootstrap = Bootstrap5(app)
 
 
 @app.route("/")
@@ -18,9 +25,15 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
     return render_template("login.html", form=login_form)
 
 
