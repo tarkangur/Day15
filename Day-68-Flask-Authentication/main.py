a841350d99a29e54d1edb06cd429e0b0ffb72907
tarkangur <142388310+tarkangur@users.noreply.github.com>
2024-06-29 19:28:00 +0300

    Update main.py

--- Day-68-Flask-Authentication/main.py
+++ Day-68-Flask-Authentication/main.py
@@ -24,11 +24,16 @@ login_manager.init_app(app)
 # CREATE TABLE IN DB
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 with app.app_context():
@@ -43,22 +48,29 @@ def home():
 @app.route('/register', methods=["POST", "GET"])
 def register():
     if request.method == "POST":
-        hash_and_salted_password = generate_password_hash(
-            password=request.form["password"],
-            method='pbkdf2:sha256',
-            salt_length=8
-        )
-        new_user = User(
-            email=request.form["email"],
-            password=hash_and_salted_password,
-            name=request.form["name"]
-        )
+        email = request.form["email"]
+        name = request.form["name"]
+        password = request.form["password"]
+        if User.query.filter_by(email=email).first():
+            flash('Email address already exists')
+        else:
+            hash_and_salted_password = generate_password_hash(
+                password=password,
+                method='pbkdf2:sha256',
+                salt_length=8
+            )
+            new_user = User(
+                email=email,
+                password=hash_and_salted_password,
+                name=name
+            )
 
-        db.session.add(new_user)
-        db.session.commit()
-        session['name'] = request.form['name']
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            session['name'] = request.form['name']
 
-        return redirect(url_for("secrets"))
+            return redirect(url_for("secrets"))
 
     return render_template("register.html")
 
@@ -68,8 +80,7 @@ def login():
     if request.method == "POST":
         email = request.form['email']
         password = request.form["password"]
-        user = db.get_or_404(User, email)
-        print(user)
+        user = User.query.filter_by(email=email).first()
         check_password = check_password_hash(password=password, pwhash=user.password)
         if check_password:
             login_user(user)
@@ -81,7 +92,6 @@ def login():
 
 
 @app.route('/secrets')
-@login_manager.user_loader
 @login_required
 def secrets():
     name = session.get('name')
@@ -90,11 +100,11 @@ def secrets():
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download', methods=["GET"])
-@login_manager.user_loader
 @login_required
 def download():
     return send_from_directory(directory='static/files', path='cheat_sheet.pdf')
